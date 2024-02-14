79df250d69e685595967b19563eb76c8bfe90f9b
Jared Hobson <jaredmhobson@gmail.com>
2024-02-14 08:32:18 -0800

    Add test for exists in course_spec

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -2,4 +2,10 @@ require 'rspec'
 require './lib/course'
 require './lib/student'
 
+RSpec.describe Course do
+  let (:course) { Course.new("Calculus", 2) }
 
+  it 'exists' do
+    expect(course).to be_instance_of(Course)
+  end
+end
