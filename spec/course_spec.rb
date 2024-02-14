6c77d09d4d97927250381e8d14c760d687ad09c0
Jared Hobson <jaredmhobson@gmail.com>
2024-02-14 08:34:30 -0800

    Add test for name method in course_spec

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,9 +3,15 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do
-  let (:course) { Course.new("Calculus", 2) }
+  let (:course) { Course.new('Calculus', 2) }
 
   it 'exists' do
     expect(course).to be_instance_of(Course)
+  end
+
+  describe '#name' do 
+    it 'has a name' do
+      expect(course.name).to eq('Calculus')
+    end
   end
 end
