3b1b5a40c11ed3c587a815c87f70aa7943cb7c9a
Jared Hobson <jaredmhobson@gmail.com>
2024-02-14 08:15:16 -0800

    Add test for name method in student_spec

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe Student do
     expect(morgan).to be_instance_of(Student)
   end
 
-  
+  describe '#name' do
+    it 'has a name' do
+      morgan = Student.new({name: "Morgan", age: 21})
+      expect(morgan.name).to eq('Morgan')
+    end
+  end
 end
