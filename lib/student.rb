eb112268f0e8ba87d1c550c2d333640c64c684d9
Jared Hobson <jaredmhobson@gmail.com>
2024-02-14 08:19:07 -0800

    Add scores to attr_reader and instance variable in student

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,9 @@
 class Student
-  attr_reader :name, :age
+  attr_reader :name, :age, :scores
 
   def initialize(student_info)
     @name = student_info[:name]
     @age = student_info[:age]
+    @scores = []
   end
 end
