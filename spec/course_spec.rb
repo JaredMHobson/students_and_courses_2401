9484e54122f4da2ec4e7e01f66d1b3c2b08d529e
Jared Hobson <jaredmhobson@gmail.com>
2024-02-14 08:42:50 -0800

    Add test for full? method in course_spec

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -4,6 +4,8 @@ require './lib/student'
 
 RSpec.describe Course do
   let (:course) { Course.new('Calculus', 2) }
+  let (:student1) { Student.new({name: "Morgan", age: 21}) }
+  let (:student2) { Student.new({name: "Jordan", age: 29}) }
 
   it 'exists' do
     expect(course).to be_instance_of(Course)
@@ -24,6 +26,17 @@ RSpec.describe Course do
   describe '#students' do
     it 'has no students by default' do
       expect(course.students).to eq([])
+    end
+  end
+
+  describe '#full?' do
+    it 'can return if it is full or not' do
+      expect(course.full?).to be false
+
+      course.enroll(student1)
+      course.enroll(student2)
+
+      expect(course.full?).to be true
     end
   end
 end
