f1cdb0f07fd43d5d89b601449b5448568d702d12
Jared Hobson <jaredmhobson@gmail.com>
2024-02-14 09:28:53 -0800

    Add list_all_students method in gradebook

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -9,4 +9,12 @@ class Gradebook
   def add_course(course)
     @courses << course
   end
+
+  def list_all_students
+    all_students = {}
+    @courses.each do |course|
+      all_students[course] = course.students
+    end
+    all_students
+  end
 end
